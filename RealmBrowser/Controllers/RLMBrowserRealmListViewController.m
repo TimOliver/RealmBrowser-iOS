5f2fbb821256f32c5660c48c7ba74393dfa8397a
Tim Oliver <me@timoliver.com.au>
2017-08-31 02:10:30 +1000

    Fixed styling issue

--- RealmBrowser/Controllers/RLMBrowserRealmListViewController.m
+++ RealmBrowser/Controllers/RLMBrowserRealmListViewController.m
@@ -310,6 +310,7 @@ NSString * const kRLMBrowserSchemaTableViewCellIdentifier = @"SchemaTableCell";
         RLMBrowserObjectListViewController *controller = [[RLMBrowserObjectListViewController alloc] initWithBrowserRealm:realm browserSchema:schema];
         UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:controller];
         [self to_showSecondaryViewController:navigationController sender:self];
+        RLM_RESET_NAVIGATION_CONTROLLER(navigationController);
     }
 }
 
